90fa1b42445d3d72c6118496ffdd269e1bddabe2
kritzcreek <christoph.hegemann1337@gmail.com>
2020-04-22 12:04:48 +0200

    Evaluierung via Closures

--- weasel/src/main/kotlin/weasel.kt
+++ weasel/src/main/kotlin/weasel.kt
@@ -1,16 +1,23 @@
+import kotlinx.collections.immutable.PersistentMap
+import kotlinx.collections.immutable.persistentHashMapOf
+
 sealed class Expr {
-    data class Plus(val summand1: Expr, val summand2: Expr): Expr()
+    data class Plus(val summand1: Expr, val summand2: Expr) : Expr()
     data class Number(val number: Int) : Expr()
     data class Var(val name: String) : Expr()
     data class Lambda(val binder: String, val body: Expr) : Expr()
+    data class Closure(val binder: String, val body: Expr, val env: Env) : Expr()
     data class Application(val func: Expr, val argument: Expr) : Expr()
 }
 
-fun eval(expr: Expr): Expr {
+typealias Env = PersistentMap<String, Expr>
+
+val emptyEnv: Env = persistentHashMapOf()
+fun eval(env: Env, expr: Expr): Expr {
     return when (expr) {
         is Expr.Plus -> {
-            val summand1 = eval(expr.summand1)
-            val summand2 = eval(expr.summand2)
+            val summand1 = eval(env, expr.summand1)
+            val summand2 = eval(env, expr.summand2)
             if (summand1 is Expr.Number && summand2 is Expr.Number) {
                 Expr.Number(summand1.number + summand2.number)
             } else {
@@ -18,47 +25,35 @@ fun eval(expr: Expr): Expr {
             }
         }
         is Expr.Number -> expr
-        is Expr.Var -> expr
-        is Expr.Lambda -> expr
-        is Expr.Application -> {
-            when (val lambda = eval(expr.func)) {
-                is Expr.Lambda -> eval(substitute(lambda.binder, eval(expr.argument), lambda.body))
-                else -> throw Exception("${lambda} is not a function")
+        is Expr.Closure -> expr
+        is Expr.Var -> env[expr.name] ?: throw Exception("${expr.name} was undefined.")
+        is Expr.Lambda -> Expr.Closure(expr.binder, expr.body, env)
+        is Expr.Application ->
+            when (val closure = eval(env, expr.func)) {
+                is Expr.Closure -> {
+                    val newEnv = closure.env.put(closure.binder, eval(env, expr.argument))
+                    eval(newEnv, closure.body)
+                }
+                else -> throw Exception("$closure is not a function")
             }
-        }
-    }
-}
-
-// Replaces all occurences of `binder` with `replacement`
-// [binder -> replacement] expr
-fun substitute(binder: String, replacement: Expr, expr: Expr): Expr {
-    return when (expr) {
-        is Expr.Plus -> Expr.Plus(
-            substitute(binder, replacement, expr.summand1),
-            substitute(binder, replacement, expr.summand2)
-        )
-        is Expr.Number -> expr
-        is Expr.Var -> if (expr.name == binder) {
-            replacement
-        } else {
-            expr
-        }
-        is Expr.Lambda -> if (expr.binder == binder) {
-            expr
-        } else {
-            Expr.Lambda(expr.binder, substitute(binder, replacement, expr.body))
-        }
-        is Expr.Application -> Expr.Application(
-            substitute(binder, replacement, expr.func),
-            substitute(binder, replacement, expr.argument)
-        )
     }
 }
 
 fun main() {
-    val expr = Expr.Application(Expr.Lambda("y", Expr.Var("y")), Expr.Number(10))
+    val expr =
+        Expr.Application(
+            Expr.Application(
+                Expr.Lambda(
+                    "y",
+                    Expr.Lambda(
+                        "x",
+                        Expr.Plus(Expr.Var("x"), Expr.Var("y"))
+                    )
+                ), Expr.Number(10)
+            ), Expr.Number(20)
+        )
     println(expr)
-    println(eval(expr))
+    println(eval(emptyEnv, expr)) // 30
 
     val complicated = Expr.Application(
         Expr.Application(
@@ -80,6 +75,6 @@ fun main() {
         Expr.Number(40)
     )
 
-    println(eval(complicated))
+    println(eval(emptyEnv, complicated)) // 42
 
 }
